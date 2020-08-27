588afdb28afc2dea47e0681be5c4c82bac660464
Armin <halilovic.armin@hotmail.com>
2020-08-27 21:34:49 +0200

    removed old unused configs

--- .PhpStorm2017.2/config/consoles/db/02cb7992-40ee-4e4d-b860-2afe9b9c835e/console.sql
+++
@@ -1,3 +0,0 @@
-update venyous set picture_headerID = null where venyous.title_nl is null and picture_headerID is not null ;
-delete from pictures where venyouID is not ;
-delete from venyous where title_nl is null;
--- .PhpStorm2017.2/config/consoles/db/b15efd55-78ec-4192-a4fb-5a35885f4a81/console.sql
+++
@@ -1,7 +0,0 @@
-update translations set is_active = 0, waiting_for_approval = 0 where key_id = 164 and language_id = 1;
-
-select * from translations where key_id = 14 and language_id = 1;
-
-update translations set is_active = 1 where id = 53;
-
-select category_id, count(*) from translations_keys group by category_id;
