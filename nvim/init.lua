b1d06bdf2200ad08fcf25b038ac3adf6f47ca6dc
Armin <armin.halilovic@cegeka.com>
2023-05-10 15:15:36 +0200

    WIP Neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+require("arminh.plugins-setup")
+require("arminh.core.options")
+require("arminh.core.keymaps")
+require("arminh.core.colorscheme")
+require("arminh.plugins.comment")
+require("arminh.plugins.nvim-tree")
---
+++ nvim/lua/arminh/core/options.lua
@@ -0,0 +1,36 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- no wrapping
+opt.wrap = false
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- consider '-' as part of a word
+opt.iskeyword:append("-")
